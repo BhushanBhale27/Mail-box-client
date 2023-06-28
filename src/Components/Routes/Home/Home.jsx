1000dd31d3e725f08bdcae261fe4fb3680550cda
BhushanBhale27 <bhushan27official@gmail.com>
2023-06-28 12:11:53 +0530

    some changes css

--- src/Components/Routes/Home/Home.jsx
+++ src/Components/Routes/Home/Home.jsx
@@ -1,17 +1,12 @@
 import React from "react";
+import '../Home/Home.css'
 
 const Home = () => {
   return (
-    <div
-      style={{
-        textAlign: "center",
-        margin: "15%",
-       
-        fontSize: "90px",
-        color: "blue",
-      }}
-    >
-      Welcome !!!
+    <div className="Home">
+      <div className="Home__content">
+        <h1 className="Home__title">Welcome !!!</h1>
+      </div>
     </div>
   );
 };
